f3284e16905c074614155392b94034a043af2a4c
brij <brijnandan@cloudbyte.com>
2016-04-06 06:17:24 +0000

    adding readme.txt
    git-svn-id: svn+ssh://10.10.1.102/svnrepo/devops@5 20142372-ea64-11e5-9694-1dd7e65929dd

--- codeanalysis/python/pylint_jenkins.sh
+++ codeanalysis/python/pylint_jenkins.sh
@@ -2,8 +2,8 @@
 #note that the path where this script should run is relative to jenkins job
 #pylint.log is created from pylint.cfg file which has all the rules defined
 #the rules which are not required can be disabled in the .cfg files.
-var1=$(grep -i \|error* pylint.log  | head -1  | cut -c32);
-var2=$(grep -i \|error* pylint.log  | head -1  | cut -c33-37);
+var1=$(grep -i \|error* pylint.log  | head -1  | cut -c32); #searching for the pattern error in pylint.log
+var2=$(grep -i \|error* pylint.log  | head -1  | cut -c33-37); #taking the  error symbols into variables
 echo $var1
 if [ $var1 == = ]
 	then
---
+++ codeanalysis/readme.txt
@@ -0,0 +1,5 @@
+This file gives the description on the various static code analysis techniques.
+the folders are created for different languages and their tools.
+
+1. Python :
+         tools: pylint
