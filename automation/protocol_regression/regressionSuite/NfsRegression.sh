eb625968a9b11f1aaebbea6fe3ea4ca4ae0c800d
mardan <mardan.singh@cloudbyte.com>
2016-08-08 06:54:25 +0000

    Rearranging Nfsregression.sh according to default run
    git-svn-id: svn+ssh://10.10.1.102/svnrepo/devops@107 20142372-ea64-11e5-9694-1dd7e65929dd

--- automation/protocol_regression/regressionSuite/NfsRegression.sh
+++ automation/protocol_regression/regressionSuite/NfsRegression.sh
@@ -3,14 +3,14 @@ python cleanup.py conf.txt
 
 python create_pool_vsms.py conf.txt
 
-#python addClientsToExports.py conf.txt filesystem_nfs
-#python removeClientFromExports.py conf.txt filesystem_nfs
+python addClientsToExports.py conf.txt filesystem_nfs
+python removeClientFromExports.py conf.txt filesystem_nfs
 python nfsSoftHardmount.py conf.txt soft
 python nfsSoftHardmount.py conf.txt hard
 python nfsSetAllDirToNo.py conf.txt filesystem_nfs
-#python TSMIPChangeofNFSdataset.py conf.txt filesystem_nfs
+python TSMIPChangeofNFSdataset.py conf.txt filesystem_nfs
 python NFSAlerts.py conf.txt
-#python NFSHomedir.py conf.txt
+python NFSHomedir.py conf.txt
 python umountUsedmountPoint.py conf.txt filesystem_nfs
 python nfsDatasetUsed_AvailSpace.py conf.txt filesystem_nfs
 python nfsReadOnlyDataset.py conf.txt filesystem_nfs
@@ -23,8 +23,14 @@ python updNFSQos.py conf.txt iopsenable 50
 python updNFSQos.py conf.txt iopsdisable 60
 python CreateMountDeletenfs.py conf.txt
 python nfs_Authorization.py conf.txt
-#python nfs_share_100_mount.py conf.txt
+
+python cleanup.py conf.txt
+python create_pool_vsms.py conf.txt
+
 python snpNFSVol.py conf.txt
-#python graceFullHANFS.py conf.txt
-#python ungraceFullHANFS.py conf.txt
+python graceFullHANFS.py conf.txt
+python ungraceFullHANFS.py conf.txt
 #python partial_fail_over_nfs.py conf.txt
+
+python cleanup.py conf.txt
+python nfs_share_100_mount.py conf.txt
