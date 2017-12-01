0820bfdecb1d67e5b4421ad50421e767bfcff617
dargasudarshan <sudarshan.darga@cloudbyte.com>
2017-12-01 12:14:56 +0530

    Update Networkloss_DR.py

--- ElastistorAutomation/SCM/TestScripts/Networkloss_DR.py
+++ ElastistorAutomation/SCM/TestScripts/Networkloss_DR.py
@@ -9,7 +9,7 @@
 #python_version  :2.7
 #==============================================================================
 
-from ES_1.GUIConfig import GuiConfig as const
+from SCM.GUIConfig import GuiConfig as const
 from SSHConnection import SSHConnection
 from WebUtils import WebUtils
 import Logging, time, sys, os
